449e83ebff78c60047c00a8e5112feded3b8d87e
Raghav Pandey <90841468+iampaandey@users.noreply.github.com>
2023-03-15 16:49:56 +0530

    Navfinal150323

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,31 @@ import logo from '../images/logo.png'
 import admin from "../images/Admin.png"
 import task from "../images/task.png"
 import dashboard from "../images/dashboard.png"
-
+import cross from "../images/cross.png"
+import  hbg from "../images/hbg.png"
 
 const Navbar = () => {
+   const showOption = ()=>{
+    document.getElementById("main-nav").style.display="flex";
+    // document.getElementById("main-nav").style.right="0px";
+    document.getElementById("main-nav").style.transition="3s";
+
+    // document.body.style.filter="blur(4.5px)"
+   }
+   const hideOptions = ()=>{
+    document.getElementById("main-nav").style.display="none";
+
+   }
+
   return (
     <>
-     <div className="main-nav">
+    <div className="phone-nav">
+      <img src={logo} alt="logophone" className='phone-logo'/>
+      <img src={hbg} alt="hbgicon" className='hbgicon' onClick={()=>{showOption()}}/>
+    </div>
+     <div className="main-nav" id='main-nav'>
        <img src={logo} alt="logo" className='logo'/>
+       <img src={cross} alt="cross" className='cross' onClick={()=>{hideOptions()}}/>
        <div className="options">
         <div  className='op'>
             <img src={dashboard} alt="dshlogo" className='dash nimg'/>
@@ -19,6 +37,7 @@ const Navbar = () => {
         <div className='op'><img src={task} alt="tasklogo" />   <h4>Tasks</h4></div>
        </div>
     </div>
+    
     </>
    
   )
