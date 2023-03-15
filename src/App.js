e25e937ef3c36975c93c46a9fc199bc60fbdee50
Piyush26iit <thepiyushkumar09@gmail.com>
2023-03-15 18:43:58 +0530

    userboxadded150323

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import UserBox from './components/UserBox';
 
 function App() {
   return (
     <>
      <Navbar/>
+     <UserBox/>
      <Dashboard/>
      <Footer/>
 
---
+++ src/components/UserBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './userbox.css'
+
+const UserBox = () => {
+  return (
+    <>
+        <div className="userbox">
+            <img src="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcSStEXQ52SE6txqvnwfAyOZ-dt6fkkBqzcir0RaZkoG54dYK7UByieR90Nb18ON4rdZ6VyDNVuQdk1kXik " alt="" />
+            <span className='username'>Raghav</span>
+        </div>
+    </>
+  )
+}
+
+export default UserBox
